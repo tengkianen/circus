cf3aaa8de5fd417236d42bd7235efc489f2a5f5c
tengkianen <tengkianen@gmail.com>
2020-09-11 16:51:47 +0800

    Updated files for arrayList

--- src/main/java/circus/animal/Bird.java
+++ src/main/java/circus/animal/Bird.java
@@ -1,10 +1,7 @@
 package circus.animal;
 
 public abstract class Bird extends Animal {
-<<<<<<< HEAD:src/main/java/circus/animal/Bird.java
 
-=======
->>>>>>> improved:src/main/java/Bird.java
     public void fly() {
         System.out.println("Whee ...");
     }
